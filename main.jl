34bbdf7763a0a1a7a35dd5a6ba59ff1193634c58
Samuele-Colombo <samuele.colombo1@studenti.unimi.it>
2021-03-03 18:23:25 +0100

    modified pakage so that all greet methods converge to the String method instead of having multiple implementations

--- main.jl
+++ main.jl
@@ -1,4 +1,4 @@
-#!/home/julia-1.5.3/bin julia
+#!~/julia-1.5.3/bin/julia
 
 using Pkg
 Pkg.activate(normpath(@__DIR__))
--- src/hello_world.jl
+++ src/hello_world.jl
@@ -1,7 +1,7 @@
 module hello_world
 
-greet() = println("Hello World!")
-greet(::Nothing) = greet()
+greet() = greet(nothing)
+greet(::Nothing) = greet("World")
 greet(msg::String) = println("Hello $(msg)!")
 greet(args::AbstractArray{<:String}) = (msg = Base.isempty(args) ? nothing : Base.join(args, " "); greet(msg))
 
